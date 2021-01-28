98cfe1948619ae4814762d5a8ec073e072fc4c6e
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-28 17:38:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,22 @@
 def greet_characters(array)
+   index = 0 
+   array.each do |element|
+   puts "Hello #{element}!"
+   index += 1 
+ end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each do |element|
-    puts "hello #{element}"
-  end
 end
 
 def list_dwarves(array)
+    index = 1
+     array.each_with_index do |element| 
+       puts "#{index} #{element}"
+       index += 1
+ end
+
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index do |value, index|
-    puts "#{index}. #{value}"
-  end  
 end
